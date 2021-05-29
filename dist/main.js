200f9282b5c82143102d240016a63c713c26b7f5
dongha <dongha@email.com>
2021-05-29 23:59:21 +0900

    feat(App.js)add : App component

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _App = _interopRequireDefault(require("./App.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = new _App["default"](document.querySelector('.app'));
